abd93a64f93634de44d21718fd3acfa780fba32e
Ashish Kumar <44310112+Ak-011@users.noreply.github.com>
2022-01-20 18:20:42 +0530

    dsalgo

---
+++ array/altufaltu.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int n;
+   for (n = 9; n!=0; n--)
+   {
+       cout<<n;
+       n--;
+   }
+   return 0;
+}
--- array/swaparray.cpp
+++ array/swaparray.cpp
@@ -1,14 +1,26 @@
 #include<iostream>
 using namespace std;
+
+void printArray(int arr[], int n){
+    for(int  i = 0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }cout<<endl;
+}
+void swapArray(int arr[], int size){
+    for(int i =0; i<size; i=i+2){
+        if(i+1<size){
+        swap(arr[i],arr[i+1]);
+        }
+    }
+}
+
 int main(){
-     int n;
-     cin>>n;
-     int arr[n];
-     for(int i =0; i<n; i++){
-         cin>>arr[i];
-     }
-     for(int i =0; i<n; i++){
-         cout<<arr[i];
-     }
-     return 0;
+    int even[8]={5,2,4,9,7,8,3,5};
+    int odd[7]={5,4,7,8,41,45,42};
+    
+    swapArray(even,8);
+    printArray(even,8);
+
+    swapArray(odd,7);
+    printArray(odd,7);
 }
