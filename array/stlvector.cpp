bfff79c8f3d2ed8631997165365587d693a83808
Ashish Kumar <44310112+Ak-011@users.noreply.github.com>
2022-01-27 11:35:09 +0530

    stl

--- array/stlvector.cpp
+++ array/stlvector.cpp
@@ -3,6 +3,48 @@
 using namespace std;
 int main(){
     vector<int>v;
-    cout<<"capacity-->"<<v.capacity<<endl;
-    
+
+    vector<int>a(5,1);
+
+    vector<int>last(a);
+    cout<<"print last"<<endl;
+    for(int i:last){
+        cout<<i<<" ";
+    }cout<<endl;
+    cout<<"print a"<<endl;
+    for(int i:a){
+        cout<<i<<" ";
+    }cout<<endl;
+    cout<<"capacity-->"<<v.capacity()<<endl;
+
+    v.push_back(1);
+    cout<<"capacity-->"<<v.capacity()<<endl;
+
+    v.push_back(2);
+        cout<<"capacity-->"<<v.capacity()<<endl; 
+
+    v.push_back(3);
+        cout<<"capacity-->"<<v.capacity()<<endl;
+        cout<<"Size-->"<<v.size()<<endl;
+
+    cout<<"Element at 2nd index is "<<v.at(2)<<endl;
+
+    cout<<v.front()<<"--"<<v.back()<<endl;
+
+    cout<<"before pop"<<endl;
+    for(int i:v){
+        cout<<i<<" ";
+    }cout<<endl;
+
+    v.pop_back();
+
+    cout<<"after pop"<<endl;
+    for(int i:v){
+        cout<<i<<" ";
+    }cout<<endl;
+    cout<<"before clear"<<v.size()<<endl;
+    v.clear();
+    cout<<"after clear"<<v.size()<<endl;
+
+
 }
