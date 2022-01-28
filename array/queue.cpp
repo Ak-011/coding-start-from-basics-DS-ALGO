07a477a1dac5f81587a5f8c3bf9a496ef57d6044
Ashish Kumar <44310112+Ak-011@users.noreply.github.com>
2022-01-28 23:44:49 +0530

    stl

---
+++ array/queue.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+int main(){
+    queue<string> q;
+    q.push("ashish");
+    q.push("kumar");
+    q.push("best");
+    cout<<"size before pop"<<q.size()<<endl;
+    cout<<"Fisrt element"<<q.front()<<endl;
+    q.pop();
+    cout<<"First element"<<q.front()<<endl;
+    
+    cout<<"size after pop"<<q.size()<<endl;
+}
