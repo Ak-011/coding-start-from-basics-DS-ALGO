a54d6eebfd068870641f39ca2365c0f1f603e5c3
Ashish Kumar <44310112+Ak-011@users.noreply.github.com>
2022-03-10 23:30:52 +0530

    linklistintro

---
+++ linklist/intro.cpp
@@ -0,0 +1,12 @@
+linear data structure made by collection of nodes 
+nodes= having some data and address of  next node 
+why link list need 
+we cant change the size of array on runtime  
+insertion and deletion is easy in linklist
+ 
+ types of link list 
+ 1.singly link list 
+ 2. doubly link list 
+ 3. Circularly link list 
+
+ 
